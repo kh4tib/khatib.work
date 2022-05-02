bf60992ca87a399be2be2d8563a2b2a8af47b57e
kh4tib <mhkhatibb@outlook.com>
2022-05-02 21:12:13 +0300

    clean up

--- app/js/pages-interaction.js
+++ app/js/pages-interaction.js
@@ -1,8 +1,8 @@
 // let aboutTarget = document.getElementsByClassName('js-about')[0];
-let humanlyTarget = document.getElementsByClassName('js-humanly')[0];
+// let humanlyTarget = document.getElementsByClassName('js-humanly')[0];
 let audiencesTarget = document.getElementsByClassName('js-audiences')[0];
-let eveTarget = document.getElementsByClassName('js-eve')[0];
-let flowTarget = document.getElementsByClassName('js-flow')[0];
+// let eveTarget = document.getElementsByClassName('js-eve')[0];
+// let flowTarget = document.getElementsByClassName('js-flow')[0];
 // let santeTarget = document.getElementsByClassName('js-sante')[0];
 // let exquisiteTarget = document.getElementsByClassName('js-exquisite')[0];
 let framesTarget = document.getElementsByClassName('js-frames')[0];
@@ -21,9 +21,9 @@ const addTheme = (target, theme) => {
 
 // addTheme(aboutTarget, '--project-theme-about');
 addTheme(audiencesTarget, '--project-theme-audiences');
-addTheme(eveTarget, '--project-theme-eve');
-addTheme(flowTarget, '--project-theme-flow');
-addTheme(humanlyTarget, '--project-theme-humanly');
+// addTheme(eveTarget, '--project-theme-eve');
+// addTheme(flowTarget, '--project-theme-flow');
+// addTheme(humanlyTarget, '--project-theme-humanly');
 addTheme(framesTarget, '--project-theme-frames');
 addTheme(dataTarget, '--project-theme-data');
 // addTheme(santeTarget, '--project-theme-sante');
